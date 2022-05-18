ce115bf54880fe4098516e291ba0848af312f5af
hunghuynh449 <hunghuynh2606@gmail.com>
2022-05-18 19:46:48 +0700

    code moi ne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { LeaderComponent } from './leader/leader.component';
+import { ListMemberComponent } from './list-member/list-member.component';
+import { InfoProjectComponent } from './info-project/info-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    LeaderComponent,
+    ListMemberComponent,
+    InfoProjectComponent
   ],
   imports: [
     BrowserModule,
